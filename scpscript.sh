0ef78456621bf92bb4a8503f7f49879864dbd51c
richellemaria <rishdsouza@gmail.com>
2024-01-09 19:35:11 +0530

    auto push

--- scpscript.sh
+++ scpscript.sh
@@ -3,20 +3,20 @@
 filename=$1
 dest=$2
 
-
+cmd= scp $filename $dest
 if [ $? -ne  0 ] ; then
     echo -n "You need to pass first argument is filename and second argument is destination details"
 
 else
-    echo -n "You have passed first argument as filename and second argument as destination details"
+    echo -n "Transfer Completed!!!You have passed first argument as filename and second argument as destination details"
 
 fi
 
 
  
-cmd= scp $filename $dest
+# cmd= scp $filename $dest
 
-if [ $? -eq 0 ] ; then
- echo -n "The file is transferred successfully $cmd"
-fi
+# if [ $? -eq 0 ] ; then
+#  echo -n "The file is transferred successfully $cmd"
+# fi
 
