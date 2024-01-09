5f9171339e77d85d65f6d28ddc5b2e75eb3502a8
richellemaria <rishdsouza@gmail.com>
2024-01-09 18:31:12 +0530

    auto push

--- scpscript.sh
+++ scpscript.sh
@@ -7,7 +7,7 @@ if [ $1 -ne 0 || $2 -ne 0 ] ; then
    echo -n "Pass the correct argumnent $1 is source details and $2 is destination details"
 
 else
-   echo "$1 is source details and $3 is destination details"
+   echo -n "$1 is source details and $2 is destination details"
 
 fi
 
