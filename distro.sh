78eb8a04586d11a9fdb8ed29fd9ae9150d9d525a
richellemaria <rishdsouza@gmail.com>
2024-01-09 22:38:54 +0530

    auto push

--- distro.sh
+++ distro.sh
@@ -3,8 +3,8 @@
 distro=$(who)
 
 echo $distro
-# if [ $distro == "centos" ] ; then
-#   echo "its a centos linux distribution"
-# elif [ $distro == "Debian" ] ; then
-#   echo "its a Debian linux distribution"
-# fi
+ if [ $distro == "^centos" ] ; then
+   echo "its a centos linux distribution"
+ elif [ $distro == "Debian" ] ; then
+   echo "its a Debian linux distribution"
+fi
