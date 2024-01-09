7a874a00b1d43702e9537919b51b10be47113807
richellemaria <rishdsouza@gmail.com>
2024-01-09 21:55:24 +0530

    auto push

--- sed_awk_script.sh
+++ sed_awk_script.sh
@@ -3,11 +3,12 @@
 filename="/etc/passwd"
 
 sedop=$(sed -n -e '5p' $filename) 
-
-
-
-awkop=$(awk -F: '{print $4}' $filename)
-
-
 echo "the 5th line of the file is ${sedop}"
-echo  "the 4th field of 5th line of the file is ${awkop}"
+
+sedop > /tmp/fiveline.txt
+
+# awkop=$(awk -F: '{print $4}' /tmp/fiveline.txt)
+
+
+
+# echo  "the 4th field of 5th line of the file is ${awkop}"
