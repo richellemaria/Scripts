900a00150a4788aa53b32e003254996563369bd0
richellemaria <rishdsouza@gmail.com>
2024-01-09 20:00:24 +0530

    auto push

--- sed_awk_script.sh
+++ sed_awk_script.sh
@@ -6,7 +6,7 @@ sedop=$(sed -n -e '5p' $filename)
 
 # awkop=awk -F: '{print $4}' 
 
-# finalop= $sedop | $awkop
+finalop=$sedop | awk -F: '{print $4}'
 
 echo -n "the 5th line of the file is ${sedop}"
-# echo -n "the 4th field of 5th line of the file is $finalop"
+echo -n "the 4th field of 5th line of the file is $finalop"
