d888e3ba7fe3c6a2acb5e77410ba6a69f86dde0e
richellemaria <rishdsouza@gmail.com>
2024-01-09 18:17:50 +0530

    auto push

--- scpscript.sh
+++ scpscript.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-filename=$1
-source=$2
-dest=$3
+source=$1
+dest=$2
 
-if [ $1 -ne 0 || $2 -ne 0 || $3 -ne 0 ] ; then
+if [ $1 -ne 0 || $2 -ne 0 ] ; then
    echo -n "Pass the correct argumnent $1 is filemane, $2 is source details and $3 is destination details"
 
 else
-   echo "$1 is filname passed ,$2 is source details and $3 is destination details"
+   echo "$1 is source details and $3 is destination details"
 
 fi
 
-cmd= scp $filename $source $dest
+cmd= scp $source $dest
 
 echo -n "The file is transferred successfully $cmd"
 
