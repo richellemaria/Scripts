45e1f8e860272fe07be7372ccab4e4cfb9040403
richellemaria <rishdsouza@gmail.com>
2024-01-09 19:14:03 +0530

    auto push

--- scpscript.sh
+++ scpscript.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-source=$1
+filename=$1
 dest=$2
 
-if [ $? -ne 0 ] ; then
-   echo -n "Pass the correct argumnent $1 is source details and $2 is destination details"
+# stat(){
+#     if [ $1 -ne 0 ] ; then
+#       echo -n "Pass the correct argumnent $1 is source details and $2 is destination details"
 
-else
-   echo -n "$1 is source details and $2 is destination details"
+#     else
+#       echo -n "$1 is source details and $2 is destination details"
 
-fi
+#     fi
+# }
 
-cmd= scp $source $dest
+cmd= scp $filename $dest
 
 if [ $? -eq 0 ] ; then
  echo -n "The file is transferred successfully $cmd"
