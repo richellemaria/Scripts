2f6cf057846b17a06fdaec20144cae14a302350b
richellemaria <rishdsouza@gmail.com>
2024-01-10 22:02:26 +0530

    auto push

--- even_odd.sh
+++ even_odd.sh
@@ -3,10 +3,9 @@
 echo "Enter a number"
 read n
 
-num = expr $n%2
-echo $num
-# if [ $num -eq 0 ] ; then
-#   echo -n "Is a even number"
-# else
-#   echo -n "Is a odd number"
-# fi
+num=$(($n % 2))
+if [ $num -eq 0 ] ; then
+  echo -n "$n Is a even number"
+else
+  echo -n "$n Is a odd number"
+fi
