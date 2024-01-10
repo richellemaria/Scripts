93a8a043451c75b9748a005099d6fec452b44681
richellemaria <rishdsouza@gmail.com>
2024-01-10 21:48:10 +0530

    auto push

--- even_odd.sh
+++ even_odd.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-num=($1/2)
-if [ $num -eq 0 ] ; then
-  echo -n "Is a even number"
-else
-  echo -n "Is a odd number"
-fi
+num=$($1/2)
+echo $num
+# if [ $num -eq 0 ] ; then
+#   echo -n "Is a even number"
+# else
+#   echo -n "Is a odd number"
+# fi
