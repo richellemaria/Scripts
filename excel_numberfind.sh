6d1ac78d0d1417d576f5b55c55589af22bfb8803
richellemaria <rishdsouza@gmail.com>
2024-01-11 17:29:19 +0530

    auto push

--- excel_numberfind.sh
+++ excel_numberfind.sh
@@ -4,4 +4,4 @@
 grep -o "[0-9]*"  /tmp/excel.xls > /tmp/number.txt
 # awk 'BEGIN {sum=0} {sum+=$1} END {print sum}' /tmp/number.txt
 
-echo -n "The Total value is $awk 'BEGIN {sum=0} {sum+=$1} END {print sum}' /tmp/number.txt
+echo -n "The Total value is $(awk 'BEGIN {sum=0} {sum+=$1} END {print sum}' /tmp/number.txt)"
